2249465662ce977a05926ec11558e74a62aec037
harry gao <foxgaocn@gmail.com>
2013-08-02 20:14:20 +1000

    tool to modify data

---
+++ tool.js
@@ -0,0 +1,21 @@
+var db = require('./lib/db.js')
+, models = require('./models/models.js');
+
+var UserHabitModel = models.userHabitModel;
+
+var startDate = process.argv[2] ? new Date(process.argv[2]) : Date.now();
+var lastUpdateDate = process.argv[3] || null;
+
+db.findUserByName("Harry Gao")
+.then(function(user){
+	console.log('user is ' + user);
+	var condition = {userId : user._id, isActive:true};
+	var update = { startDate:startDate, lastUpdateDate: lastUpdateDate };
+
+	UserHabitModel.update(condition, update, null, function(err, numAffected){
+		if(err) console.log(err);
+		else console.log(numAffected + " documents have been changed");
+	})
+
+}).done();
+
