7607e1224336aa4b3d7c2501c025b0f04c720f40
harry gao <foxgaocn@gmail.com>
2013-09-01 16:07:35 +1000

    style

--- controllers/habit/index.js
+++ controllers/habit/index.js
@@ -1,22 +1,27 @@
 var db =require ('./../../lib/db.js');
 
+//AJAX 
 exports.create = function(req, res, next){
-	db.createHabit(req.body.trigger, req.body.action, req.body.goal).
-	then(function(habit){
-		db.createUserHabit(req.user._id, habit._id,  req.body.startDate, null, true)
-		.then(function(user_habit){
-			res.send('done');
+	//marking existing active habit as inactive
+	db.deactivateUserHabit(req.user._id)
+	.then(
+		db.createHabit(req.body.trigger, req.body.action, req.body.goal)
+		.then(function(habit){
+			db.createUserHabit(req.user._id, habit._id,  req.body.startDate, null, true)
+			.then(function(user_habit){
+				res.send('done');
+				})
 		})
-	}).catch( function(err){
-		res.send('error');
-	})
-	.done();
+		.catch( function(err){
+				res.send('error');
+		})
+		).done();
 }
 
+//AJAX
 exports.update = function(req, res, next){
 	console.log('upating habit lastUpdateDate ');
-	console.log('lastUpdateDate ' + req.body.lastUpdateDate);
-	db.updateUserHabit(req.user._id, req.body.lastUpdateDate).
+	db.updateLastUpdateDate(req.user._id, req.body.lastUpdateDate).
 	then(function(){
 		console.log('update done');
 		res.send('done');
