2a85f78d58242a40c6d3c5acc284f145443ae05c
Haiyang (harryg) Gao <harrygao.oz@gmail.com>
2013-09-08 19:38:00 +1000

    add prod config

--- configure.js
+++ configure.js
@@ -7,7 +7,7 @@ var func = function(){
     				appId: '144772812392904'
   					, appSecret: '90c2e3d9829758a8c6bbc6b0990146fe'
 				}
-    			, database: 'mongodb://127.0.0.1/habitor'
+    			, database: 'mongodb://127.0.0.1/habitized_dev'
     			, cookieSecret: '342kfsdakj'
     		};
 
@@ -18,6 +18,16 @@ var func = function(){
   					, appSecret: '90c2e3d9829758a8c6bbc6b0990146fe'
 				}
     			, database: 'mongodb://127.0.0.1/habitor_test'
+    			, cookieSecret: '342kfsdakj'
+    		};
+
+    	case 'product' :
+    		return {
+    			fb: {
+    				appId: '294012080740019'
+  					, appSecret: '6415e225774416988a5ce5d2a6bf822d'
+				}
+    			, database: 'mongodb://127.0.0.1/habitized'
     			, cookieSecret: '342kfsdakj'
     		};
 
